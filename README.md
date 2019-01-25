d7a90aa722f3d1e0fc68dbd1ea527cf72fab1f72
walkeralance65@gmail.com <walkerlance65@gmail.com>
2019-01-24 22:39:38 -0700

    v2.0.0
    Schema

--- README.md
+++
@@ -1,4 +0,0 @@
-# CLS-CodeLifeContracting-Website 
-  ### Software | Personal Software Portfolio, Custom Resource Hub, and Project Manager.
-  ### URL: www.codelifecontracts.co
-
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+// ============================================================
+// CodeLifeContracting Website
+// main.go
+// ============================================================
+// Created by Lance T. Walker on 1/24/2019
+// Copyright 2019 CodeLife-Productions. All rights reserved.
+// ============================================================
+
+import (
+	"fmt"
+
+	// local:
+	// Backend "./backend"
+	// 	Routes 		"./backend/routes"
+	// 	Services 	"./backend/services"
+	// 	usecases 	"./backend/usecases"
+	// 	Utilities "./backend/utilities"
+)
+
+/*** Server Properties ***/
+const (
+	// _Backend = Backend
+	_FrontendPath string = "./frontend"
+)
+
+func main() {
+  fmt.Printf("STARTING");
+}
